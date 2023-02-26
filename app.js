62fc652d76d26205eed584ead44c2a8244897198
basshhii0610 <117554796+basshhii0610@users.noreply.github.com>
2023-02-26 10:27:26 +0900

    いろいろ

--- app.js
+++ app.js
@@ -27,12 +27,21 @@ app.get("/", (req, res) => {
     res.render("top.ejs");
 });
 
-app.get("/rankings", (req, res) => {
-    res.render("rankings.ejs");
-});
-
-app.get("/community", (req, res) => {
-    res.render("community.ejs");
+app.get("/leaderboard", (req, res) => {
+    connection.query(
+        "SELECT RANK() OVER(ORDER BY pp DESC) Ranking, country, name, acc, plays AS \"playcount\", pp, xh_count + sh_count AS \"SS\", x_count + s_count AS \"S\", a_count AS \"A\"" +
+        "FROM users" +
+        "JOIN stats" +
+        "ON users.id = stats.id" +
+        "WHERE mode = ?" +
+        "AND NOT users.id = 1" +
+        "ORDER BY pp DESC;",
+        [0],
+        (error, results) => {
+            console.log(results);
+            res.render("leaderboard.ejs")
+        }
+    )
 });
 
 app.listen(3000);
