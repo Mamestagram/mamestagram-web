44b54256226e5c21f68e0280a912ce14ee3268f4
basshhii0610 <117554796+basshhii0610@users.noreply.github.com>
2023-03-06 12:58:35 +0900

    関数の追加

--- app.js
+++ app.js
@@ -17,6 +17,34 @@ app.use(
 
 
 
+const getModeNum = (sp, mode) => {
+    if (sp === "none") {
+        switch (mode) {
+            case "std":
+                return 0;
+            case "taiko":
+                return 1;
+            case "ctb":
+                return 2;
+            case "mania":
+                return 3;
+        }
+    }
+    else if (sp === "rx") {
+        switch (mode) {
+            case "std":
+                return 4;
+            case "taiko":
+                return 5;
+            case "ctb":
+                return 6;
+        }
+    }
+    else if (sp === "ap") {
+        return 8;
+    }
+}
+
 app.get("/", (req, res) => {
    res.redirect("/home");
 });
@@ -27,40 +55,9 @@ app.get("/home", (req, res) => {
 
 app.get("/leaderboard/mode=:mode/special=:sp", (req, res) => {
     let mode_num;
-    if (req.params.sp === "none") {
-        switch (req.params.mode) {
-            case "std":
-                mode_num = 0;
-                break;
-            case "taiko":
-                mode_num = 1;
-                break;
-            case "ctb":
-                mode_num = 2;
-                break;
-            case "mania":
-                mode_num = 3;
-                break;
-        }
-    }
-    else if (req.params.sp === "rx") {
-        switch (req.params.mode) {
-            case "std":
-                mode_num = 4;
-                break;
-            case "taiko":
-                mode_num = 5;
-                break;
-            case "ctb":
-                mode_num = 6;
-                break;
-        }
-    }
-    else if (req.params.sp === "ap") {
-        mode_num = 8;
-    }
+    mode_num = getModeNum(req.params.sp, req.params.mode);
     connection.query(
-        "SELECT RANK() OVER(ORDER BY pp DESC) ranking, country, name, acc, plays, pp, " +
+        "SELECT users.id AS 'id', RANK() OVER(ORDER BY pp DESC) ranking, country, name, acc, plays, pp, " +
         "xh_count + x_count AS 'SS', sh_count + s_count AS 'S', a_count AS 'A' " +
         "FROM users " +
         "JOIN stats " +
@@ -89,7 +86,82 @@ app.get("/login" ,(req, res) => {
 });
 
 app.get("/profile/id=:id/mode=:mode/special=:sp", (req, res) => {
-    res.render("profile.ejs");
+    let mode_num;
+    const query = new Array(4);
+    mode_num = getModeNum(req.params.sp, req.params.mode);
+    connection.query(
+        "SELECT name, " +
+        "(SELECT COUNT(*) + 1 " +
+        "  FROM stats " +
+        "  WHERE pp > (" +
+        "      SELECT pp " +
+        "      FROM stats " +
+        "      WHERE id = ? " +
+        "      AND mode = ?)" +
+        "  AND mode = ?) AS 'GRanking', " +
+        "(SELECT COUNT(*) + 1 " +
+        "  FROM stats " +
+        "  WHERE pp > ( " +
+        "      SELECT pp " +
+        "      FROM stats " +
+        "      WHERE id = ? " +
+        "      AND mode = ?)" +
+        "  AND mode = ?) AS 'CRanking', " +
+        "acc, plays, total_hits, rscore, tscore, replay_views, pp, playtime " +
+        "FROM stats " +
+        "JOIN users " +
+        "ON stats.id = users.id " +
+        "WHERE users.id = ? " +
+        "AND mode = ?;",
+        [req.params.id, mode_num, mode_num, req.params.id, mode_num, mode_num, req.params.id, mode_num],
+        (error, results) => {
+            query[0] = results[0];
+        }
+    );
+    connection.query(
+        "SELECT grade, title, artist, mods, acc, pp " +
+        "FROM scores " +
+        "JOIN maps " +
+        "ON map_md5 = md5 " +
+        "WHERE userid = ? " +
+        "AND scores.mode = ? " +
+        "AND NOT grade = 'F' " +
+        "ORDER BY pp DESC " +
+        "LIMIT 100;",
+        [req.params.id, mode_num],
+        (error, results) => {
+            query[1] = results;
+        }
+    );
+    connection.query(
+        "SELECT title, artist, creator, maps.mode, COUNT(*) AS 'playtimes' " +
+        "FROM scores " +
+        "JOIN maps " +
+        "ON map_md5 = md5 " +
+        "WHERE userid = ? " +
+        "GROUP BY map_md5 " +
+        "ORDER BY playtimes DESC;",
+        [req.params.id],
+        (error, results) => {
+            query[2] = results;
+        }
+    );
+    connection.query(
+        "SELECT grade, title, artist, scores.mode, mods, acc, pp " +
+        "FROM scores " +
+        "JOIN maps " +
+        "ON map_md5 = md5 " +
+        "WHERE userid = ? " +
+        "AND TIMEDIFF(CURRENT_TIMESTAMP(), play_time) <= '24:00:00' " +
+        "AND NOT grade = 'F' " +
+        "ORDER BY play_time DESC;",
+        [req.params.id],
+        (error, results) => {
+            query[3] = results;
+            res.render("profile.ejs", {info: query[0], bestpp: query[1],
+                bestplaytimes: query[2], recentplays: query[3]});
+        }
+    );
 });
 
 app.listen(3000);
